51dcbab152012c72cfee247a3ba06476ab0efd9c
sanyamsharma25 <sanyamvats25@gmail.com>
2023-09-01 14:15:44 +0530

    final commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// document.writeln('<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.16/typed.umd.js" integrity="sha512-+2pW8xXU/rNr7VS+H62aqapfRpqFwnSQh9ap6THjsm41AxgA0MhFRtfrABS+Lx2KHJn82UOrnBKhjZOXpom2LQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>');
+// document.writeln('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>');
+// document.writeln('<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>');
+
+// var script = document.createElement('script');
+// script.src = 'https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js';
+// var typed=new Typed(".typing",{
+//     strings:["Web Designer","Web Developer","Software Developer","Video Editor"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
+
+
